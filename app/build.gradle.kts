6ccea4f16a845f4de3471f0b71bdad2b110d76c5
CLettuce <passipan02@gmail.com>
2023-10-26 00:42:49 -0600

    Jumping views and scrolling added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ android {
     namespace = "com.example.jobshunters"
     compileSdk = 34
 
+
     defaultConfig {
         applicationId = "com.example.jobshunters"
         minSdk = 24
@@ -40,4 +41,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/example/jobshunters/MainActivity.java
+++ app/src/main/java/com/example/jobshunters/MainActivity.java
@@ -1,42 +1,46 @@
 package com.example.jobshunters;
-
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import com.example.jobshunters.databinding.MainActivityBinding;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    MainActivityBinding binding;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = MainActivityBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.main_activity);
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-            switch (item.getItemId()){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame_main, new HomeFragment())
+                .commit();
 
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
 
+            if (itemId == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.favorite) {
+                selectedFragment = new FavoritosFragment();
+            } else if (itemId == R.id.info) {
+                selectedFragment = new AcercaDeFragment();
+            } else if (itemId == R.id.book) {
+                selectedFragment = new RecursoFragment();
+            }
 
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_main, selectedFragment)
+                        .commit();
             }
 
             return true;
         });
     }
-    private void replaceFragment(Fragment fragment){
-
-        FragmentManager fragmentManger = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManger.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_main, fragment);
-        fragmentTransaction.commit();
-    }
-
 }
+
+
+
